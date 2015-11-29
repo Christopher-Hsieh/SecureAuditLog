aa626e2e9b1d592d7310aef2223c372e035cf079
Sam Presnal <spresnal@purdue.edu>
2015-11-29 16:02:47 -0600

    Added helper.c

---
+++ helper.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+
+/*
+    functions that are shared b/t trusted & untrusted
+*/
+
+char *createKey(int length) {
+    static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";        
+
+    char *currentKey = NULL;
+     currentKey = malloc(sizeof(char) * (length +1));
+
+    if (currentKey) {    
+      	int n;        
+        for (n = 0; n < length; n++) {            
+            int key = rand() % (int)(sizeof(charset) -1);
+            currentKey[n] = charset[key];
+        }
+
+        currentKey[length] = '\0';
+    }
+
+    return currentKey;
+}
--- trusted.c
+++ trusted.c
@@ -7,7 +7,13 @@
 #include <openssl/ssl.h>
 #include <openssl/bio.h>
 
+#include "prototypes.h"
+
 char* getCertificate(char* publicKey){
 	//not sure how to generate the certificate or what the paper is even saying for this part
 	return "random_key";
+}
+
+char* verifyLog(char* log){
+	
 }
